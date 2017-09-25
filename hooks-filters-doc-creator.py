f03a1b900e48e15f1d2dc949c8ed0afd1dc1be3e
Frank Corso <frank@frankcorso.me>
2017-09-25 19:27:16 -0400

    Moves get all function to locate module

--- hooks-filters-doc-creator.py
+++ hooks-filters-doc-creator.py
@@ -9,31 +9,20 @@ import sys
 
 import locate
 import github
+import models
 
 
 def clear_screen():
 	os.system("cls" if os.name == 'nt' else 'clear')
 
 
-def get_hooks_filters(user, repo, files):
+def get_hooks_filters(user, repo):
 	"""Gets all hooks and filters from files (path strings)"""
-	hooks = dict()
-	filters = dict()
-	for file in files:
-		print("Opening file: {}...".format(file))
-		file_contents = github.get_file_lines(user, repo, file)
-		hooks_filters = locate.locate_hooks_filters(file_contents)
-		hooks.update(hooks_filters['hooks'])
-		filters.update(hooks_filters['filters'])			
-	return hooks, filters
+	return locate.locate_all_hooks_filters(user, repo)
 
 if __name__ == '__main__':
 	clear_screen()
-	if (len(sys.argv) > 1):
-		project_files = github.get_paths(sys.argv[1], sys.argv[2])
-		hooks, filters = get_hooks_filters(sys.argv[1], sys.argv[2], project_files)
-		print("We found {} hooks and {} filters!".format(len(hooks), len(filters)))
-		for hook, desc in hooks.items():
-			print("{}: {}".format(hook, desc))
-		for filter, desc in filters.items():
-			print("{}: {}".format(filter, desc))
+	models.initialize()
+	if len(sys.argv) > 0:
+		hooks, filters = get_hooks_filters(sys.argv[1], sys.argv[2])
+		print("Found {} hooks and {} filters".format(len(hooks), len(filters)))
