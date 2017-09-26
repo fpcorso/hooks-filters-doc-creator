36e01b3233146c97793cf60e7f773ee0dcc3ca8a
Frank Corso <frank@frankcorso.me>
2017-09-25 20:16:53 -0400

    Creates route for displaying hooks/filters

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, flash
+import json
 
 import models
 import forms
@@ -11,12 +12,21 @@ app.secret_key = config.SECRET_KEY
 
 @app.route('/', methods=('GET', 'POST'))
 def main_page():
-	"""Single route in the Flask app. Shows a form that can be used to add repo to queue"""
+	"""Main page. Shows a form that can be used to add repo to queue"""
 	form = forms.AddRepoForm()
 	if form.validate_on_submit():
 		flash("New repo added!")
 		models.Queue.create_queue_entry(form.user.data, form.repo.data, form.email.data)
 	return render_template('index.html', form=form)
+
+
+@app.route('/id', methods=('GET', 'POST'))
+def repo_page():
+	"""Page for a repo. Shows hooks and filters of the repo."""
+	repo = models.Queue.select().where(Queue.id == id).get()
+	hooks = json.loads(repo.hooks)
+	filters = json.loads(repo.filters)
+	return render_template('repo.html', repo=repo.repo, hooks=hooks, filters=filters)
 
 
 if __name__ == '__main__':
